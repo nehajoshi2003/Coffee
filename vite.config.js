c3c715ef3dba4da0a9371c6f06dfb7d5691f3ec0
Neha Joshi <nehajoshi@Nehas-Laptop.local>
2025-02-19 22:08:02 +0530

    Fix missing CSS/JS files in GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Coffee/",  // Add this line
+  base: "/.",  // Add this line
   plugins: [react()],
   server: {
     hmr: false  // Disables hot reload to prevent constant reloading
