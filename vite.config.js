3e75f389f527b1293fc553fceac25251e3179ab1
Neha Joshi <nehajoshi@Nehas-Laptop.local>
2025-02-19 21:58:05 +0530

    make it live

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Coffee/",  // Add this line
   plugins: [react()],
   server: {
     hmr: false  // Disables hot reload to prevent constant reloading
   }
 })
-
-
